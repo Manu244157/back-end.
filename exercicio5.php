5ca404cfd93a8a5e58964c174473c83fd17793e1
Manu244157 <emanuela.luiz.senai404@gmail.com>
2024-08-16 09:34:13 -0300

    Add files via upload

---
+++ exercicio5.php
@@ -0,0 +1,18 @@
+<?php
+$tabu="Tabuada";
+echo "$tabu <br>";
+
+//Declarando a função
+function vezes($num){
+    //Chamando o for para a função
+    for($i = 1; $i <= 10; $i++) {
+        $resultado = $num * $i;
+        //coloquei este echo para mostrar tal texto no site
+        echo "$num x $i = $resultado<br>";
+    }
+}
+//declarando o valor da tabuada
+$num = 2;
+vezes($num); 
+
+?>
