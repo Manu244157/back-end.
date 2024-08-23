3abfb444ae8c7b10ad6b9e17a94b0be5f4c71221
Manu244157 <emanuela.luiz.senai404@gmail.com>
2024-08-23 09:16:01 -0300

    Update exercicio8.php

--- exercicio8.php
+++ exercicio8.php
@@ -2,7 +2,7 @@
 //Declarando variavel soma
 $soma = 0;
 
-for($num = 1; $num <= 100; $num++) { //Definindoa sequencia de números de 1 a 100
+for($num = 1; $num <= 100; $num++) { //Definindo sequencia de números de 1 a 100
     $resultado = $soma; 
     $soma+=$num; //Fazendo a soma
     echo " A soma é : $resultado + $num = $soma<br>";  //Exibindo no site o resultado da soma
