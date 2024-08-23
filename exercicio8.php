bdb13f67fa1a520889915a251c20bbcbd5b45610
Manu244157 <emanuela.luiz.senai404@gmail.com>
2024-08-23 09:15:35 -0300

    Add files via upload

---
+++ exercicio8.php
@@ -0,0 +1,11 @@
+<?php
+//Declarando variavel soma
+$soma = 0;
+
+for($num = 1; $num <= 100; $num++) { //Definindoa sequencia de números de 1 a 100
+    $resultado = $soma; 
+    $soma+=$num; //Fazendo a soma
+    echo " A soma é : $resultado + $num = $soma<br>";  //Exibindo no site o resultado da soma
+
+}
+?>
