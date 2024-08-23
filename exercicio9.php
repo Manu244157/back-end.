423a6e76d15e67bacdb9dd39eedb44ed961299e2
Manu244157 <emanuela.luiz.senai404@gmail.com>
2024-08-23 09:46:48 -0300

    Update exercicio9.php
    Botei a raiz quadrada para 2 casas decimais

--- exercicio9.php
+++ exercicio9.php
@@ -3,7 +3,7 @@
 
 for($num = 1; $num <= 50; $num++) { //Definindoa sequencia de números de 1 a 50
     
-    $resultado = sqrt($num); //Fazendo a  raiz quadrada
+     $resultado = round(sqrt($num),2); //Fazendo a  raiz quadrada
     echo " A raiz quadrada de   $num é : $resultado <br>";  //Exibindo no site o resultado 
 
 }
