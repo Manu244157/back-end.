745ce98326b5be8de6a41c670ccf2bd20e64d502
Manu244157 <emanuela.luiz.senai404@gmail.com>
2024-10-02 16:07:58 -0300

    Update válida_login.php

--- app_help_desk/válida_login.php
+++ app_help_desk/válida_login.php
@@ -1,24 +1,33 @@
 <?php
 
 $usuario_auntenticado = false ; 
-
+//colocando usuario certo
 $usuario_cadastrado = [
     ['email' => 'admin@senai.br',
     'senha'  => '12345'
-    ]
+    ],
+     ['email' => 'aluno@senai.br',
+    'senha'  => 'abcde'
+    ],
+     ['email' => 'suporte@senai.br',
+    'senha'  => '12345'
+    ],
 ];
 
+//fazendo verificação
 foreach ($usuario_cadastrado as $user){
     if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_auntenticado = true;
     }
 }
 
-
+//verificação do usuario 
 if ($usuario_auntenticado == true){
     echo "Usuário autenticado com sucesso";
+    header('Location: painel.php');
 }else{
     echo "Usuário ou senha incorreto";
+    header('Location: index.php?erro=user');
 }
 ?>
     
