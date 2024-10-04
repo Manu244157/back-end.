95a131ed0ea6a5ef1f50e277c894f7618359f83c
Manu244157 <emanuela.luiz.senai404@gmail.com>
2024-10-04 11:21:15 -0300

    Update index.php

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -3,6 +3,9 @@
     <meta charset="utf-8" />
     <title>App Help Desk</title>
 
+    <link rel="stylesheet" type="text/css" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+
     <link rel="stylesheet" type="text/css" href="styles.css">
 
     </style>
@@ -26,13 +29,29 @@
               Login
             </div>
             <div class="card-body">
-              <form>
+              <form action="válida_login.php" method="post">
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+                  <input name= 'email' type="email" class="form-control" placeholder="E-mail" require>
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
+                  <input name='senha' type="password" class="form-control" placeholder="Senha" require>
                 </div>
+
+                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro'){?>
+
+                <div class="text-danger">
+                  Usuário ou senha inválido(s)
+                </div>
+
+                <?php } ?>
+                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro2'){?>
+
+                <div class="text-danger">
+                  Faça login antes de acessar as páginas
+                </div>
+
+                <?php } ?>
+               
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
