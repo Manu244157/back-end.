d991282123ee14a33b502b3fc42be8e7b0e81e30
Manu244157 <emanuela.luiz.senai404@gmail.com>
2024-10-04 11:23:32 -0300

    Update válida_login.php

--- app_help_desk/válida_login.php
+++ app_help_desk/válida_login.php
@@ -1,34 +1,35 @@
 <?php
+session_start();
+$usuario_autenticado = false;
 
-$usuario_auntenticado = false ; 
-//colocando usuario certo
+
+
 $usuario_cadastrado = [
-    ['email' => 'admin@senai.br',
-    'senha'  => '12345'
-    ],
-     ['email' => 'aluno@senai.br',
-    'senha'  => 'abcde'
-    ],
-     ['email' => 'suporte@senai.br',
-    'senha'  => '12345'
-    ],
+    ['email'=> 'admin@senai.br',
+    'senha'=> 12345],
+    ['email'=> 'aluno@senai.br',
+    'senha'=> 'abcde'],
+    ['email'=> 'suporte@senai.br',
+    'senha'=> '1a2b3c']
 ];
 
-//fazendo verificação
-foreach ($usuario_cadastrado as $user){
-    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
-        $usuario_auntenticado = true;
-    }
-}
-
-//verificação do usuario 
-if ($usuario_auntenticado == true){
-    echo "Usuário autenticado com sucesso";
-    header('Location: painel.php');
-}else{
-    echo "Usuário ou senha incorreto";
-    header('Location: index.php?login=erro');
-}
-?>
+foreach($usuario_cadastrado as $user){
     
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){ $usuario_autenticado = true; }
+       
 
+
+}
+
+if($usuario_autenticado){
+    $_SESSION['autenticado'] = 'SIM';
+    header ('location: painel.php');
+    }
+else{
+    $_SESSION['autenticado'] = 'NÃO';
+    header ('location: index.php?login=erro');
+}
+   
+
+
+?>
